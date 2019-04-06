f930ca365569985a5038e2258157dc171ed94dbc
Arijit Patra <ari.clicks@gmail.com>
2019-04-06 15:18:29 +0530

    save of new sport
    save of new sport

--- src/client/container/create-sport-request/create-sport-request.js
+++ src/client/container/create-sport-request/create-sport-request.js
@@ -6,6 +6,20 @@ import { Button, Checkbox, Form } from "semantic-ui-react";
 class CreateSportRequest extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: "",
+      sportsName: sessionStorage.getItem("sportsSelected"),
+      availability: "",
+      charge: "",
+      ground: "",
+      playTimestamp: "",
+      postedOn: new Date().toISOString(),
+      postedBy: "Michael Kapoor",
+      age: 43,
+      gender: "Male",
+      phone: "9999999999",
+      type: "sports"
+    };
   }
 
   goToSportsList = () => {
@@ -13,44 +27,94 @@ class CreateSportRequest extends React.Component {
     history.push("/sportsList");
   };
 
+  handleInputChange(key, value) {
+    console.log(key, value);
+    this.setState({ [key]: value });
+  }
+
+  saveNewSportRequest = () => {
+    this.goToSportsList();
+  };
+
   render() {
+    let { title, availability, charge, ground, playTimestamp } = this.state;
     return (
       <React.Fragment>
         <h1 className="mt-4 mb-5">Create post</h1>
-
         <div className="row mt-3 mb-5">
           <div className="col-12">
             <Form>
               <Form.Field>
                 <label>Title</label>
-                <input placeholder="Title" />
+                <input
+                  placeholder="Title"
+                  name="title"
+                  type="text"
+                  value={title}
+                  onChange={event =>
+                    this.handleInputChange("title", event.target.value)
+                  }
+                />
               </Form.Field>
               <Form.Field>
                 <label>Sport</label>
-                <select>
+                <select
+                  onChange={event =>
+                    this.handleInputChange("sport", event.target.value)
+                  }
+                >
                   <option value="football">Football</option>
+                  <option value="cricket">cricket</option>
                 </select>
               </Form.Field>
               <Form.Field>
                 <label>Availability</label>
-                <input placeholder="Availability" />
+                <input
+                  placeholder="Availability"
+                  name="availability"
+                  value={availability}
+                  onChange={event =>
+                    this.handleInputChange("availability", event.target.value)
+                  }
+                />
               </Form.Field>
               <Form.Field>
                 <label>Charge</label>
-                <input placeholder="Charge" />
+                <input
+                  placeholder="Charge"
+                  name="charge"
+                  value={charge}
+                  onChange={event =>
+                    this.handleInputChange("charge", event.target.value)
+                  }
+                />
               </Form.Field>
               <Form.Field>
                 <label>Ground</label>
-                <input placeholder="Ground" />
+                <input
+                  placeholder="Ground"
+                  name="ground"
+                  value={ground}
+                  onChange={event =>
+                    this.handleInputChange("ground", event.target.value)
+                  }
+                />
               </Form.Field>
               <Form.Field>
                 <label>Time</label>
-                <input placeholder="Time" />
+                <input
+                  placeholder="Time"
+                  name="playTimestamp"
+                  value={playTimestamp}
+                  onChange={event =>
+                    this.handleInputChange("playTimestamp", event.target.value)
+                  }
+                />
               </Form.Field>
               <Button
                 type="submit"
                 className="bg-success text-white"
-                onClick={this.goToSportsList}
+                onClick={this.saveNewSportRequest}
               >
                 Create A Listing
               </Button>
